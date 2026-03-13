e359633a33367315585bb67c62a6f1bdf83cf740
M A A Z <104779758+maazm7d@users.noreply.github.com>
2026-03-13 14:58:49 +0530

    GetHallOfFameMembersUseCase: replace loadMembers with observeMembers

--- app/src/main/java/com/maazm7d/termuxhub/domain/usecase/GetHallOfFameMembersUseCase.kt
+++ app/src/main/java/com/maazm7d/termuxhub/domain/usecase/GetHallOfFameMembersUseCase.kt
@@ -2,11 +2,12 @@ package com.maazm7d.termuxhub.domain.usecase
 
 import com.maazm7d.termuxhub.data.repository.HallOfFameRepository
 import com.maazm7d.termuxhub.domain.model.HallOfFameMember
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetHallOfFameMembersUseCase @Inject constructor(
     private val repository: HallOfFameRepository
 ) {
-    suspend operator fun invoke(): List<HallOfFameMember> =
-        repository.loadMembers()
+    operator fun invoke(): Flow<List<HallOfFameMember>> =
+        repository.observeMembers()
 }
